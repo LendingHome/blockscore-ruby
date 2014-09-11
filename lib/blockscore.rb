1e0b468f226a02c8f931b6dcd1873b2f84faa19f
Alain Meier <me@alainmeier.com>
2014-09-11 15:36:03 -0700

    Require files manually

--- lib/blockscore.rb
+++ lib/blockscore.rb
@@ -1,5 +1,17 @@
-require "httparty"
+require 'httparty'
+require 'hashie'
 
-Dir[File.dirname(__FILE__) + '/blockscore/*.rb'].each do |file|
-  require file
-end
+require_relative 'blockscore/restful.rb'
+require_relative 'blockscore/base.rb'
+require_relative 'blockscore/company.rb'
+require_relative 'blockscore/errors.rb'
+require_relative 'blockscore/question_set.rb'
+require_relative 'blockscore/verification.rb'
+require_relative 'blockscore/watchlist.rb'
+require_relative 'blockscore/watchlist_candidate.rb'
+
+module BlockScore
+  class << self
+    attr_accessor :api_key
+  end
+end 
